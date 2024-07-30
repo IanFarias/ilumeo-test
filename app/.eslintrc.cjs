5a1aeb2dbc6a5a870c6e4c8239a8d266d19bc02f
Ian Farias Paixão <ianfariaspaixao14@gmail.com>
2024-07-29 22:15:05 -0300

    adding eslint config app

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier-recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react-refresh", "@typescript-eslint", "prettier", "react"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/react-in-jsx-scope": 0,
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -82,7 +82,7 @@ const Home: React.FC = () => {
       </S.DisplaySection>
       {!active && (
         <Button type="button" onClick={handleAction}>
-          Registrar entradas
+          Registrar entrada
         </Button>
       )}
 
